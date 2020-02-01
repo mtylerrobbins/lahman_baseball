4a85176322ce7a3725a25f8838feb16df81ae206
Tyler <mtylerrobbinsgmail.com>
2020-02-01 10:06:18 -0600

    question 4 ans

--- data/bb_putout_pos.sql
+++ data/bb_putout_pos.sql
@@ -2,5 +2,11 @@
 players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as
 "Infield", and those with position "P" or "C" as "Battery". 
 Determine the number of putouts made by each of these three groups in 2016.*/
-
+SELECT (
+	CASE WHEN pos ILIKE '%SS%'OR pos ILIKE '%1B%' OR pos ILIKE '%2B%' OR pos ILIKE '%3B%' THEN 'Infield'
+		WHEN pos ILIKE 'P%' OR pos ILIKE 'C%' THEN 'Battery'
+		ELSE 'Outfield'END) AS pos_group, COUNT(playerid) AS count_pl, SUM(PO) AS putouts
+FROM fielding
+WHERE yearID = 2016
+GROUP BY pos_group
 
